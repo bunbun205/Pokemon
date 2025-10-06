83dbac9ea81ba5917967d555aaf761a0aaaefed2
bunbun205 <mayank69123@gmail.com>
2025-10-06 23:36:42 +0530

    First Assignment

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,5 +2,12 @@
 
 int main() {
 
+    std::string playerName;
+
+    std::cout << "Enter your name: ";
+    std::cin >> playerName;
+
+    std::cout << "Great start " << playerName << ", looks like you have understood the main() function properly now!" << std::endl;
+
     return 0;
 }
